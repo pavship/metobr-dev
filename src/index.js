e8f8b10c4d6508123c4f030aebf16f5505a02017
Pavel Shipitsyn <pavship@yandex.ru>
2018-10-05 12:22:11 +0300

    with Apollo client

--- src/index.js
+++ src/index.js
@@ -4,7 +4,32 @@ import 'semantic-ui-css/semantic.min.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+
+// @ts-ignore
+const client = new ApolloClient({
+	uri: 'http://localhost:4000',
+	// uri: 'https://now-advanced.now.sh',
+	// uri: 'https://env-1542080.mircloud.ru',
+	// request: (operation) => {
+	// 	const token = localStorage.getItem(AUTH_TOKEN)
+	// 	operation.setContext({
+	// 		headers: {
+	// 			Authorization: token ? `Bearer ${token}` : '',
+	// 		}
+	// 	})
+	// }
+})
+
+ReactDOM.render(
+	<ApolloProvider client={client}>
+		<App
+      // token={token}
+      client={client}
+    />
+	</ApolloProvider>
+, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
